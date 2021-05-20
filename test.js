1e5e0e717e1e739cfb60ca55069446cd721f1e99
konupall <50708658+konupall@users.noreply.github.com>
2021-05-20 13:30:09 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,2 @@
 document.getElementsByClassName("igra sama-igra ighra-test").item("").getElementsByClassName("container").item("").getElementsByClassName("error_text text-center").item(0).classList.item("value").replace("error_text", "retarded_website")
+// jooksuta kood ja vajuta soorita test
