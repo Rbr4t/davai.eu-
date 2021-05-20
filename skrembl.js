270af89f8999fb06dd4c8808951c288e79fde736
konupall <50708658+konupall@users.noreply.github.com>
2021-05-20 13:30:27 +0300

    Update skrembl.js

--- skrembl.js
+++ skrembl.js
@@ -1,4 +1,4 @@
-// copy this to chrome console
+// see läheb chrome konsooli
 var intervalId = window.setInterval(function(){
   activeBox = document.getElementsByClassName("one_slovesnoe_chodo shown").item("").getElementsByClassName("bukovki_yakubovicha").item("").getElementsByClassName("one_bukovka one_bukovka_pole active")
     activeBox.item("").innerHTML = "\n" + activeBox.item("").getAttribute("data-val") + "\n\n"
