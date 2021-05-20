ac9243f3f5be33d02e680f617db2c522b1753385
konupall <50708658+konupall@users.noreply.github.com>
2021-05-20 13:26:05 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,1 @@
+document.getElementsByClassName("igra sama-igra ighra-test").item("").getElementsByClassName("container").item("").getElementsByClassName("error_text text-center").item(0).classList.item("value").replace("error_text", "retarded_website")
