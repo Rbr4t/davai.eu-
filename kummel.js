eddfd1cb7b638c6e8bfc2c33ab06c22e5f65e03c
konupall <50708658+konupall@users.noreply.github.com>
2021-05-20 12:53:52 +0300

    Create kummel.js

---
+++ kummel.js
@@ -0,0 +1,4 @@
+// kummel unlimited mistakes
+// pms spami ei tea ja sa võidad ez
+let source = document.getElementsByClassName("col-sm-4 col-game3-image").item(0).children.romashka
+source.setAttribute("data-errors", -10000)
