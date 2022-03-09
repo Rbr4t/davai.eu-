ce34f299aa00ef0ef630d394f550a046a7cfa67f
Rbr4t <95379567+Rbr4t@users.noreply.github.com>
2022-03-09 20:57:01 +0200

    ühenda_paarid
    tegin veel koodi et teha ära ka paaride ülesanne

---
+++ sisesta_sõna.js
@@ -0,0 +1,2 @@
+// sisesta see jupp koodi concole ja vali järgmine ülesanne
+params = new URLSearchParams(document.location.search.substring(1)); playgame($('.user_photo').attr('du'), params.get('igra'), 116, '.game3_inner')
