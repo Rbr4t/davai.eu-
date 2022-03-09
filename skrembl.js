98b9ee5a6d2b710bce9dae8a98e54b8a507c99b0
Rbr4t <95379567+Rbr4t@users.noreply.github.com>
2022-03-09 20:53:06 +0200

    Uuendus

--- skrembl.js
+++ skrembl.js
@@ -3,3 +3,6 @@ var intervalId = window.setInterval(function(){
   activeBox = document.getElementsByClassName("one_slovesnoe_chodo shown").item("").getElementsByClassName("bukovki_yakubovicha").item("").getElementsByClassName("one_bukovka one_bukovka_pole active")
     activeBox.item("").innerHTML = "\n" + activeBox.item("").getAttribute("data-val") + "\n\n"
 }, 1000);
+
+// uus kood selle tegmiseks, läheb samamoodi konsooli. Spämmi ei tea ja asi ants :)
+params = new URLSearchParams(document.location.search.substring(1)); playgame($('.user_photo').attr('du'), params.get('igra'), $('#game_four_results').attr('id-game'), '.game4_inner')
